00e01fde3a642cc923f1098398d69397bf5b7126
bagortechnology <hello.bagortech@gmail.com>
2023-05-06 18:06:47 +0800

    modified:   resources/views/admin/app.blade.php

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -171,8 +171,8 @@
     </div>
 </div>
 
-<script src="dist/js/scripts.js"></script>
-<script src="dist/js/custom.js"></script>
+<script src="{{ asset('dist/js/scripts.js') }}"></script>
+<script src="{{ asset('dist/js/custom.js') }}"></script>
 
 </body>
 </html>
