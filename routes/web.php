ff08d8f95aee7d88d3c2ea60b4be1f8b75ca921d
bagortechnology <hello.bagortech@gmail.com>
2023-05-08 13:09:15 +0800

    modified:   resources/views/admin/login.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,5 @@ Route::get('/', function () {
 /* Admin */
 Route::get('admin/home', [AdminHomeController::class, 'index'])->name('admin_home');
 Route::get('admin/login', [AdminLoginController::class, 'index'])->name('admin_login');
+Route::post('admin/login-submit', [AdminLoginController::class, 'login_submit'])->name('admin_login_submit');
 Route::get('admin/forget-password', [AdminLoginController::class, 'forget_password'])->name('admin_forget_password');
