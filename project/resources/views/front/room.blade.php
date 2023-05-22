4fa16fbc059f4ec318db99ed16959d0be06667eb
bagortechnology <hello.bagortech@gmail.com>
2023-05-22 16:12:46 +0800

    Working on New Dynamic Design

--- project/resources/views/front/room.blade.php
+++ project/resources/views/front/room.blade.php
@@ -12,10 +12,30 @@
     </div>
 </div>
 
-<div class="home-rooms">
-    <div class="container">
+<div class="home-rooms_all">
+    <div class="container my-3">
+        @foreach($room_all as $item)
         <div class="row">
-            @foreach($room_all as $item)
+            <div class="col-md-4 mb-3">
+                <img src="{{ asset('uploads/' .$item->featured_photo) }}" class="img-fluid" alt="Room Image">
+              </div>
+              <div class="col-md-4">
+                <h3><a href="{{ route('room_detail',$item->id) }}">{{ $item->name }}</a></h3>
+                <ul>
+                  <li><i class="bi bi-bed"></i> Queen Size Bed</li>
+                  <li><i class="bi bi-bed"></i> Twin Beds</li>
+                </ul>
+              </div>
+              <div class="col-md-4">
+                <h4>Price Details</h4>
+                <p>Starting from $200 per night</p>
+                <a href="#" class="btn btn-primary">Book Now</a>
+                <a href="#" class="btn btn-secondary">Learn More</a>
+              </div>
+            </div>
+        @endforeach
+        </div>
+            {{-- @foreach($room_all as $item)
             <div class="col-md-3">
                 <div class="inner">
                     <div class="photo">
@@ -32,12 +52,12 @@
                     </div>
                 </div>
             </div>
-            @endforeach
+            @endforeach --}}
 
-            <div class="col-md-12">
+            {{-- <div class="col-md-12">
                 {{ $room_all->links() }}
-            </div>
-        </div>
-    </div>
+            </div> --}}
+
+
 </div>
 @endsection
